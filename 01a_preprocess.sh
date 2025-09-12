8080f4c7c8281da2c6b9027fa18d853e1a2d4bb7
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2025-09-12 12:41:58 +0200

    minor changes

--- 01a_preprocess.sh
+++ 01a_preprocess.sh
@@ -12,5 +12,5 @@ fi
 touch $DATA_FOLDER/interim/$EXP_TITLE/reads.fasta
 
 for file in "$SRC_FOLDER"/*.gz; do
-    zcat "$file" | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' >> data/interim/S1AvsAAPF_20June2025/reads.fasta
+    zcat "$file" | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' >> $DATA_FOLDER/interim/$EXP_TITLE/reads.fasta
 done
